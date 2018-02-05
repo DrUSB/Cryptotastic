463c961ac7cb806749fdd3497a00902bffff08bf
DrUSB <salimia2@uni.coventry.ac.uk>
2018-02-05 22:43:24 +0000

    Updated and added eslint file for linting

--- scripts.js
+++ scripts.js
@@ -8,12 +8,12 @@
         var requestURL = 'https://api.coinmarketcap.com/v1/ticker/';
         var request = new XMLHttpRequest();
   
-        request.open('GET',requestURL);
+        request.open('GET',requestURL,true);
 
         request.responseType = 'json';
         request.send();
         request.onload = function() {
-            var prices = request.response;
+        var prices = request.response;
             showPrices(prices);
         }
 
