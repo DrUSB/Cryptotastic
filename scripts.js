572a1f0291809769e89371fe7488e6d201347b6b
DrUSB <salimia2@uni.coventry.ac.uk>
2018-02-05 21:18:33 +0000

    Added script file

---
+++ scripts.js
@@ -0,0 +1,55 @@
+        var tablePrices = document.querySelector('table');
+        var section = document.querySelector('section');
+        var tr = document.querySelector('tr');
+
+        var imgs = ['https://www.livecoinwatch.com/images/icons32/btc.png','https://www.livecoinwatch.com/images/icons32/eth.png'];
+        var container = document.getElementById('imageContainer');
+
+        var requestURL = 'https://api.coinmarketcap.com/v1/ticker/';
+        var request = new XMLHttpRequest();
+  
+        request.open('GET',requestURL);
+
+        request.responseType = 'json';
+        request.send();
+        request.onload = function() {
+            var prices = request.response;
+            showPrices(prices);
+        }
+
+        function showPrices(jsonObj){
+          
+            for(var j = 0;j<50;j++){
+                var row = document.createElement('tr');
+                var price = document.createElement('td');
+                var name = document.createElement('td');
+                var pc24h = document.createElement('td');
+               
+        
+                    name.textContent = jsonObj[j].name;
+                    price.textContent ='$' + jsonObj[j].price_usd;
+                    pc24h.textContent = jsonObj[j].percent_change_24h + '%';
+                    
+                    row.appendChild(name);
+                    row.appendChild(price);
+                    row.appendChild(pc24h);
+                    tablePrices.appendChild(row);
+
+                }
+
+
+
+   
+
+
+
+        }
+
+        function showImages(imgs){
+            for (var i = 0; i < imgs.length; i++){
+                var img = document.createElement('img');
+                img.src = imgs[i];
+                container.appendChild(img);
+            }
+
+        }
